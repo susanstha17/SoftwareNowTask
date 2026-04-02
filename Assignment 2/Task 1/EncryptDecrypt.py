5674030d043940a91dc3f1926c42b952a91d6c00
Mr-RawV <ravikarkey@gmail.com>
2026-04-02 23:35:05 +1100

    Add comments to clarify main function and file reading process

--- Assignment 2/Task 1/EncryptDecrypt.py
+++ Assignment 2/Task 1/EncryptDecrypt.py
@@ -1,7 +1,11 @@
+#main function
 def main():
     print("Hello, World!")
+    # Read the contents of the file "raw_text.txt"
     with open("/InputOutput/raw_text.txt", "r") as file:
         data = file.read()
         print(data)
+
+# call the main function
 if __name__ == "__main__":
     main()
