ec3490347f8dd2ee1f21bbd0757dd007ad101ff2
Mr-RawV <ravikarkey@gmail.com>
2026-04-02 23:44:20 +1100

    Fix file path for reading raw text in EncryptDecrypt script

--- Assignment 2/Task 1/EncryptDecrypt.py
+++ Assignment 2/Task 1/EncryptDecrypt.py
@@ -1,8 +1,10 @@
+import os
+print(os.getcwd())
 #main function
 def main():
     print("Hello, World!")
     # Read the contents of the file "raw_text.txt"
-    with open("/InputOutput/raw_text.txt", "r") as file:
+    with open("Assignment 2/Task 1/InputOutput/raw_text.txt", "r") as file:
         data = file.read()
         print(data)
 
