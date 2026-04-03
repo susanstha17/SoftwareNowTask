f3e0a6242fb5cb8c0460e62b4a9d905933eafc5e
Mr-RawV <ravikarkey@gmail.com>
2026-04-04 00:08:57 +1100

    Refactor EncryptDecrypt script to improve file handling and add encryption start message; create encrypted_text.txt file

--- Assignment 2/Task 1/EncryptDecrypt.py
+++ Assignment 2/Task 1/EncryptDecrypt.py
@@ -1,17 +1,25 @@
 import os
 print(os.getcwd())
+
+# encryption function
+def encrypt(raw_text, shift1, shift2):
+    # check if the file exists
+    if not os.path.exists(raw_text):
+        print(f"File {raw_text} does not exist.")
+        return
+    # Read the contents of the file "raw_text.txt"
+    with open(raw_text, "r") as file:
+        data = file.read()
+        print(data)
+    print(f'Encryption of {raw_text} has started with shift values {shift1} and {shift2}')
+
 #main function
 def main():
     print("Hello, World!")
-    file_path = "Assignment 2/Task 1/InputOutput/raw_text.txt"
-    # check if the file exists
-    if not os.path.exists(file_path):
-        print(f"File {file_path} does not exist.")
-        return
-    # Read the contents of the file "raw_text.txt"
-    with open(file_path, "r") as file:
-        data = file.read()
-        print(data)
+    raw_text = "Assignment 2/Task 1/InputOutput/raw_text.txt"
+    encrypted_text = "Assignment 2/Task 1/InputOutput/encrypted_text.txt"
+    decrypted_text = "Assignment 2/Task 1/InputOutput/decrypted_text.txt"
+    
     # ask user for two shift values
     shift1 = int(input("Enter first shift value (1-25): "))
     # validate first shift value
@@ -23,6 +31,16 @@ def main():
     if shift2 < 1 or shift2 > 25:
         print("Invalid shift value. Please enter a value between 1 and 25.")
         return
+    # Encrypt the data using the shift values
+    encrypted_data = encrypt(raw_text, shift1, shift2)
+    # with open(encrypted_text, "w") as file:
+    #     file.write(encrypted_data)
+    # print("Data encrypted successfully.")
+    # # Decrypt the data
+    # decrypted_data = decrypt(encrypted_data, shift1, shift2)
+    # with open(decrypted_text, "w") as file:
+    #     file.write(decrypted_data)
+    # print("Data decrypted successfully.")
 
 # call the main function
 if __name__ == "__main__":
